bb06ec0e4c82b49a96caa41e6fea5c899b8239dd
lahaymd <lahaymd@gmail.com>
2018-08-29 08:01:27 -0500

    updated for mobile

--- client/src/FilterRoute.js
+++ client/src/FilterRoute.js
@@ -889,7 +889,7 @@ console.log(newArray);
                 <div className="App">
                 <div className='grid-svg-filterdata'>
                
-                    <svg className='item-a' xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox='0 0 500 500' width='100%' height='100%' preserveAspectRatio='xMidYMin meet'>
+                    <svg className='item-a' xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox='0 0 500 500' width='100%' height='100%' preserveAspectRatio='none'>
                             {this.state.selectedSourceGraphic == 'text' ? (
                                 <SourceGraphic
                                     text={Object.values(this.state.SourceGraphicAttrs[11])}
